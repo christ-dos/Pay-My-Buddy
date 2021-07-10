7c1903bc71a78b4f83b4377c4f711584ec72aba0
Christine Duarte <christine_dos_santos@hotmail.fr>
2021-07-10 17:08:19 +0200

    implementation model user implemantation method find all in userService

--- src/main/java/com/openclassrooms/paymybuddy/model/User.java
+++ src/main/java/com/openclassrooms/paymybuddy/model/User.java
@@ -1,13 +1,29 @@
 package com.openclassrooms.paymybuddy.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "utilisateur")
-public class User {
+import lombok.Data;
 
+@Entity
+@Table(name="user")
+@Data
+public class User {
+    @Id
+    @Column(name="email")
     private String email;
+
+    @Column(name="mot_passe")
     private String password;
-    
+
+    @Column(name="solde_compte")
+    private Double accountBalance;
+
+    @Column(name="numero_compte_bancaire")
+    private Integer bankAccountNumber;
+
+    @Column(name="amis_email")
+    private String friendEmail;
 }
---
+++ src/main/java/com/openclassrooms/paymybuddy/service/UserService.java
@@ -0,0 +1,20 @@
+package com.openclassrooms.paymybuddy.service;
+
+import com.openclassrooms.paymybuddy.model.User;
+import com.openclassrooms.paymybuddy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Iterable<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+
+}
