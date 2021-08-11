e081015085f275d0b89d99d5fcd19b35e9385f20
Christine Duarte <christine_dos_santos@hotmail.fr>
2021-08-12 00:41:24 +0100

    implementation method addTransfer ant tests when balance is enough or not enough and when transfer is credit

--- src/main/java/com/openclassrooms/paymybuddy/service/TransferService.java
+++ src/main/java/com/openclassrooms/paymybuddy/service/TransferService.java
@@ -1,11 +1,14 @@
 package com.openclassrooms.paymybuddy.service;
 
+import com.openclassrooms.paymybuddy.exception.BalanceInsufficientException;
 import com.openclassrooms.paymybuddy.model.Transfer;
 import com.openclassrooms.paymybuddy.model.User;
 import com.openclassrooms.paymybuddy.repository.ITransferRepository;
 import com.openclassrooms.paymybuddy.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class TransferService implements ITransferService {
@@ -20,12 +23,21 @@ public class TransferService implements ITransferService {
         this.userRepository = userRepository;
     }
 
-
+    @Transactional
     @Override
     public Transfer addTransfer(Transfer transfer) {
         User currentUser = userRepository.findByEmail(transfer.getUserEmail());
-        //upadate of balanec after transfer
-        currentUser.setBalance(currentUser.getBalance() + transfer.getAmount());
+        //update of balance when transfer type is debit
+        if (transfer.getType().equalsIgnoreCase("debit")) {
+            if (currentUser.getBalance() > transfer.getAmount()) {
+                currentUser.setBalance(currentUser.getBalance() - transfer.getAmount());
+            } else {
+                throw new BalanceInsufficientException("Service: transfer not possible, because the balance is insufficient");
+            }
+        } else {
+            //update of balance when transfer type is credit
+            currentUser.setBalance(currentUser.getBalance() + transfer.getAmount());
+        }
         transfer.setUser(currentUser);
         return transferRepository.save(transfer);
     }
--- src/test/java/com/openclassrooms/paymybuddy/service/TransferServiceTest.java
+++ src/test/java/com/openclassrooms/paymybuddy/service/TransferServiceTest.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.paymybuddy.service;
 
+import com.openclassrooms.paymybuddy.exception.BalanceInsufficientException;
 import com.openclassrooms.paymybuddy.model.Transfer;
 import com.openclassrooms.paymybuddy.model.User;
 import com.openclassrooms.paymybuddy.repository.ITransferRepository;
@@ -11,9 +12,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TransferServiceTest {
@@ -32,24 +35,60 @@ public class TransferServiceTest {
         transferServiceTest = new TransferService(transferRepositoryMock, userRepositoryMock);
     }
 
-
     @Test
-    public void addTransferTest_whenUserEmailIsDada_thenVerifyBalanceIs120(){
+    public void addTransferTest_whenUserEmailIsDadaAndTransferTypeIsCredit_thenVerifyBalanceIs120AndUserEmailIsDada() {
         //GIVEN
         User user = User.builder()
-                .email("dada@email.fr").firstName("Damien").lastName("Sanches").balance(20.0).accountBank(589632)
+                .email("dada@email.fr").firstName("Damien").lastName("Sanchez").balance(20.0).accountBank(589632)
                 .build();
-        Transfer transferTest = Transfer.builder().transferId(1)
+        Transfer transferTestCredit = Transfer.builder().transferId(1)
                 .amount(100.0).date(LocalDateTime.now()).type("credit").userEmail("dada@email.fr").build();
-        when(transferRepositoryMock.save(isA(Transfer.class))).thenReturn(transferTest);
-        when(userRepositoryMock.findByEmail(transferTest.getUserEmail())).thenReturn(user);
+        when(transferRepositoryMock.save(isA(Transfer.class))).thenReturn(transferTestCredit);
+        when(userRepositoryMock.findByEmail(transferTestCredit.getUserEmail())).thenReturn(user);
         //WHEN
-        Transfer transferResult = transferServiceTest.addTransfer(transferTest);
+        Transfer transferResult = transferServiceTest.addTransfer(transferTestCredit);
         //THEN
         assertEquals("dada@email.fr", transferResult.getUserEmail());
-        assertEquals(100,transferResult.getAmount());
+        assertEquals(100, transferResult.getAmount());
         assertEquals("credit", transferResult.getType());
-        assertEquals(589632,transferResult.getUser().getAccountBank());
+        assertEquals(589632, transferResult.getUser().getAccountBank());
         assertEquals(120.0, transferResult.getUser().getBalance());
+        verify(transferRepositoryMock, times(1)).save(isA(Transfer.class));
     }
+
+    @Test
+    public void addTransferTest_whenTransferTypeIsDebitAndBalanceIsEnough_thenTransferIsSavedAndBalanceUpdatedTo50() {
+        //GIVEN
+        User user = User.builder()
+                .email("dada@email.fr").firstName("Damien").lastName("Sanchez").balance(100.0).accountBank(589632)
+                .build();
+        Transfer transferTestDebit = Transfer.builder().transferId(1)
+                .amount(50.0).date(LocalDateTime.now()).type("debit").userEmail("dada@email.fr").build();
+        when(transferRepositoryMock.save(isA(Transfer.class))).thenReturn(transferTestDebit);
+        when(userRepositoryMock.findByEmail(transferTestDebit.getUserEmail())).thenReturn(user);
+        //WHEN
+        Transfer transferResultDebitBalanceEnough = transferServiceTest.addTransfer(transferTestDebit);
+        //THEN
+        //new balance is updated
+        assertEquals("debit", transferResultDebitBalanceEnough.getType());
+        assertEquals(50, transferResultDebitBalanceEnough.getUser().getBalance());
+        assertEquals("dada@email.fr", transferTestDebit.getUserEmail());
+        verify(transferRepositoryMock, times(1)).save(isA(Transfer.class));
+    }
+
+    @Test
+    public void addTransferTest_whenTransferTypeIsDebitAndBalanceInsufficient_thenTrowBalanceInsufficientException() {
+        //GIVEN
+        User user = User.builder()
+                .email("dada@email.fr").firstName("Damien").lastName("Sanchez").balance(49.0).accountBank(589632)
+                .build();
+        Transfer transferTestDebitBalanceInsufficient = Transfer.builder().transferId(1)
+                .amount(50.0).date(LocalDateTime.now()).type("debit").userEmail("dada@email.fr").build();
+        when(userRepositoryMock.findByEmail(transferTestDebitBalanceInsufficient.getUserEmail())).thenReturn(user);
+        //WHEN
+        //THEN
+        assertThrows(BalanceInsufficientException.class, () -> transferServiceTest.addTransfer(transferTestDebitBalanceInsufficient));
+        verify(transferRepositoryMock, times(0)).save(transferTestDebitBalanceInsufficient);
+    }
+
 }
