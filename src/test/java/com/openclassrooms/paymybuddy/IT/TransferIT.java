44bfe6ff8e5d20db746ecece2f27276a929ca9ed
Christine Duarte <christine_dos_santos@hotmail.fr>
2021-09-06 14:52:32 +0200

    cleaning code in application-test.properties, TransactionIT, TransferIT.java and UserIT

--- src/test/java/com/openclassrooms/paymybuddy/IT/TransferIT.java
+++ src/test/java/com/openclassrooms/paymybuddy/IT/TransferIT.java
@@ -3,8 +3,6 @@ package com.openclassrooms.paymybuddy.IT;
 import com.openclassrooms.paymybuddy.model.TransferTypeEnum;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
